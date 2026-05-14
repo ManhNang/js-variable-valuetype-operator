a2f35dbf06640c8e088f45ea1bf2060e4a6e1a17
ManhNang <manhnang12d@gmail.com>
2026-05-14 09:49:44 +0700

    add all file

---
+++ Sử dụng Toán tử/app.js
@@ -0,0 +1,4 @@
+let inputWidth = parseInt(prompt("Enter the width"));
+let inputHeight = parseInt(prompt("Enter the height"));
+let dienTich = inputHeight * inputWidth;
+alert(`Diện tích hình chữ nhật: ${dienTich}`);
